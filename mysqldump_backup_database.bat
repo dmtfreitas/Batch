de01ca64e08dc41add6afa7d2612519007828be2
Demétrio de Freitas Oliveira <81397961+dmtfreitas@users.noreply.github.com>
2026-07-29 14:09:20 -0300

    Update mysqldump_backup_database.bat

--- mysqldump_backup_database.bat
+++ mysqldump_backup_database.bat
@@ -11,7 +11,7 @@ set /p ur="user: "
 set /p db="Database: "
 for %%m in ("%mysqldump_path%") do (cd /d "%%~m")
 for %%b in ("%~dp0backup_%db%.sql") do (mysqldump --column-statistics=0 -h %ht% -P %Pt% -u %ur% -p %db% > "%%~b" && cls)
-echo "Backup successfully completed!" 
+echo Backup successfully completed!
 pause > nul
 goto :exit
 :exit
